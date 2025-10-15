96ac1f1d10ab927d53df2dcb983b5227d504de34
henmeyer <henriquemmeyer@gmail.com>
2025-10-15 20:53:07 -0300

    Dev: add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+#
+
+## Seeds for Local Development
+return if Rails.env.production?
+
+user = User.create!(name: 'Signer', email: 'signer@rails-docsign-api.com')
+document = Document.create!(title: 'Car Purchase Document', status: :signed, user: user)
+Signature.create!(signer_email: user.email, document: document, status: :completed)
+Document.create!(title: 'Loft Purchase Document', status: :draft, user: user)
